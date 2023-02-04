372efc97819b9cd3ec8bcf0d3f8bc29f573de2ca
NewChoBo <whwjdrb1009@gmail.com>
2023-02-04 14:28:37 +0900

    카드 모양 꾸미기 Transform? clipBehavior? overflow?

--- lib/main.dart
+++ lib/main.dart
@@ -110,6 +110,7 @@ class App extends StatelessWidget {
                 height: 20,
               ),
               Container(
+                clipBehavior: Clip.hardEdge,
                 decoration: BoxDecoration(
                   color: const Color(0xFF1F2123),
                   borderRadius: BorderRadius.circular(20),
@@ -117,6 +118,7 @@ class App extends StatelessWidget {
                 child: Padding(
                   padding: const EdgeInsets.all(30),
                   child: Row(
+                    mainAxisAlignment: MainAxisAlignment.spaceBetween,
                     children: [
                       Column(
                         crossAxisAlignment: CrossAxisAlignment.start,
@@ -144,14 +146,27 @@ class App extends StatelessWidget {
                               const SizedBox(
                                 width: 5,
                               ),
-                              Text('EUR',
-                                  style: TextStyle(
-                                    color: Colors.white.withOpacity(0.8),
-                                  )),
+                              Text(
+                                'EUR',
+                                style: TextStyle(
+                                  color: Colors.white.withOpacity(0.8),
+                                ),
+                              ),
                             ],
                           )
                         ],
                       ),
+                      Transform.scale(
+                        scale: 2.2,
+                        child: Transform.translate(
+                          offset: const Offset(-5, 12),
+                          child: const Icon(
+                            Icons.euro_rounded,
+                            color: Colors.white,
+                            size: 88,
+                          ),
+                        ),
+                      )
                     ],
                   ),
                 ),
