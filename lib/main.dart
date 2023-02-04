4eccf29da82d713fb34ea512f40bd5d06dd4be39
NewChoBo <whwjdrb1009@gmail.com>
2023-02-04 22:04:09 +0900

    #4.4 Widget Lifecycle (07:18)
    라이프싸이클

--- lib/main.dart
+++ lib/main.dart
@@ -10,6 +10,14 @@ class App extends StatefulWidget {
 }
 
 class _AppState extends State<App> {
+  bool showTitle = true;
+
+  void toggleTitle() {
+    setState(() {
+      showTitle = !showTitle;
+    });
+  }
+
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
@@ -23,8 +31,12 @@ class _AppState extends State<App> {
         body: Center(
           child: Column(
             mainAxisAlignment: MainAxisAlignment.center,
-            children: const [
-              MyLargeTitle(),
+            children: [
+              showTitle ? const MyLargeTitle() : const Text('nothing'),
+              IconButton(
+                onPressed: toggleTitle,
+                icon: const Icon(Icons.remove_red_eye),
+              ),
             ],
           ),
         ),
@@ -33,13 +45,37 @@ class _AppState extends State<App> {
   }
 }
 
-class MyLargeTitle extends StatelessWidget {
+class MyLargeTitle extends StatefulWidget {
   const MyLargeTitle({
     Key? key,
   }) : super(key: key);
 
   @override
+  State<MyLargeTitle> createState() => _MyLargeTitleState();
+}
+
+class _MyLargeTitleState extends State<MyLargeTitle> {
+  //상태를 초기화하기 위한 메소드. 직접 할 수도 있다. 대부분은 필요없다.
+  //하지만, 종종 부모에 의존하는 데이터를 초기화하게 될 수 있음.
+
+  //initState 메소드는 build 메소드보다 먼저 호출된다.
+  //initState는 단 한번만 호출된다.
+  //initState를 호출할때는 부모생성자를 반드시 동작시켜야 한다.
+  @override
+  void initState() {
+    super.initState();
+    print('initState!');
+  }
+
+  @override
+  void dispose() {
+    super.dispose();
+    print('dispose!');
+  }
+
+  @override
   Widget build(BuildContext context) {
+    print('build!');
     return Text(
       'My Large Title',
       style: TextStyle(
