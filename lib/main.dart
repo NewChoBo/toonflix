3ea72b318cfdb630e63141653053ffdef5bc0df5
NewChoBo <whwjdrb1009@gmail.com>
2023-02-05 15:30:07 +0900

    #5.0 User Interface (13:35)
    화면 비율처리.
    안드로이드 스튜디오에서 weight 주면서 했던거랑 비슷한 것으로 보임.

--- lib/main.dart
+++ lib/main.dart
@@ -1,90 +1,24 @@
 import 'package:flutter/material.dart';
+import 'package:toonflix/screens/home_screen.dart';
 
 void main() {
   runApp(App());
 }
 
-class App extends StatefulWidget {
-  @override
-  State<App> createState() => _AppState();
-}
-
-class _AppState extends State<App> {
-  bool showTitle = true;
-
-  void toggleTitle() {
-    setState(() {
-      showTitle = !showTitle;
-    });
-  }
-
+class App extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
       theme: ThemeData(
+        backgroundColor: const Color(0xFFE7626C),
         textTheme: const TextTheme(
-          titleLarge: TextStyle(color: Colors.red),
-        ),
-      ),
-      home: Scaffold(
-        backgroundColor: const Color(0xFFF4EDDB),
-        body: Center(
-          child: Column(
-            mainAxisAlignment: MainAxisAlignment.center,
-            children: [
-              showTitle ? const MyLargeTitle() : const Text('nothing'),
-              IconButton(
-                onPressed: toggleTitle,
-                icon: const Icon(Icons.remove_red_eye),
-              ),
-            ],
+          headline1: TextStyle(
+            color: Color(0xFF232B55),
           ),
         ),
+        cardColor: const Color(0xFFF4EDDB),
       ),
-    );
-  }
-}
-
-class MyLargeTitle extends StatefulWidget {
-  const MyLargeTitle({
-    Key? key,
-  }) : super(key: key);
-
-  @override
-  State<MyLargeTitle> createState() => _MyLargeTitleState();
-}
-
-class _MyLargeTitleState extends State<MyLargeTitle> {
-  //상태를 초기화하기 위한 메소드. 직접 할 수도 있다. 대부분은 필요없다.
-  //하지만, 종종 부모에 의존하는 데이터를 초기화하게 될 수 있음.
-
-  //initState 메소드는 build 메소드보다 먼저 호출된다.
-  //initState는 단 한번만 호출된다.
-  //initState를 호출할때는 부모생성자를 반드시 동작시켜야 한다.
-  @override
-  void initState() {
-    super.initState();
-    print('initState!');
-  }
-
-  @override
-  void dispose() {
-    super.dispose();
-    print('dispose!');
-  }
-
-  @override
-  Widget build(BuildContext context) {
-    print('build!');
-    return Text(
-      'My Large Title',
-      style: TextStyle(
-        fontSize: 30,
-        color: Theme.of(context)
-            .textTheme
-            .titleLarge!
-            .color, //null 방지를 위해 느낌표를 안붙이면 동작하지 않는다.
-      ),
+      home: const HomeScreen(),
     );
   }
 }
