03774793c77713ac889cd820cb16aadb305929c0
NewChoBo <whwjdrb1009@gmail.com>
2023-02-05 16:12:03 +0900

    #5.1 Timer (05:48) UI 만든것에 타이머 기능 살짝 섞어주었지만, 버튼을 누를때마다 타이머 함수가 호출되어 여러번 누르면 배로 시간이 빨리가는 현상

--- lib/screens/home_screen.dart
+++ lib/screens/home_screen.dart
@@ -1,3 +1,5 @@
+import 'dart:async';
+
 import 'package:flutter/material.dart';
 
 class HomeScreen extends StatefulWidget {
@@ -8,6 +10,25 @@ class HomeScreen extends StatefulWidget {
 }
 
 class _HomeScreenState extends State<HomeScreen> {
+  int totalSeconds = 1500;
+  late Timer timer; //late는 당장 property를 초기화하지 않는다.
+
+  void onTick(Timer timer) {
+    setState(() {
+      totalSeconds--;
+    });
+  }
+
+  void onStartPressed() {
+    //타이머는 Dart 표준 라이브러리에 포함되어 있다.
+    timer = Timer.periodic(
+      const Duration(seconds: 1),
+      onTick,
+    ); //periodic은 주기적으로 함수를 실행한다.
+    //함수에 괄호는 넣지 않는다. 그것은 함수를 지금 실행하는것을 의미한다(?)
+    //함수는 timer 스스로를 인자로 가지고 간다.
+  }
+
   @override
   Widget build(BuildContext context) {
     return Scaffold(
@@ -19,7 +40,7 @@ class _HomeScreenState extends State<HomeScreen> {
             child: Container(
               alignment: Alignment.bottomCenter,
               child: Text(
-                '25:00',
+                '$totalSeconds',
                 style: TextStyle(
                   color: Theme.of(context).cardColor,
                   fontSize: 89,
@@ -34,7 +55,7 @@ class _HomeScreenState extends State<HomeScreen> {
               child: IconButton(
                 iconSize: 120,
                 color: Theme.of(context).cardColor,
-                onPressed: () {},
+                onPressed: onStartPressed,
                 icon: const Icon(Icons.play_circle_outline),
               ),
             ),
@@ -46,8 +67,17 @@ class _HomeScreenState extends State<HomeScreen> {
                 Expanded(
                   child: Container(
                     decoration: BoxDecoration(
-                      color: Theme.of(context).cardColor,
-                    ),
+                        color: Theme.of(context).cardColor,
+
+                        //border Radius 방법
+                        // borderRadius: BorderRadius.circular(50),
+                        // borderRadius: const BorderRadius.only(
+                        //   topLeft: Radius.circular(50),
+                        //   topRight: Radius.circular(50),
+                        // ),
+                        borderRadius: const BorderRadius.vertical(
+                          top: Radius.circular(40),
+                        )),
                     child: Column(
                       mainAxisAlignment: MainAxisAlignment.center,
                       children: [
